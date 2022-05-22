9b30684787d11e16178db7e7a5dd3d6a2d805937
Arafath-Rahman <mkhan172093@bscse.uiu.ac.bd>
2022-05-22 22:23:56 +0600

    added express cors dotenv mongo to project, basic server structure added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//start
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Manufacture app listening on port ${port}`)
+})
