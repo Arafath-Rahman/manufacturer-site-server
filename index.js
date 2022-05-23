896e881c5f6a7a6fd1a7e50349f614ff1d7eac6c
Arafath-Rahman <mkhan172093@bscse.uiu.ac.bd>
2022-05-23 23:54:56 +0600

    setup mongodb, created DB and made a get route for all parts available

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,10 +10,37 @@ app.use(cors());
 app.use(express.json());
 
 
+//DB configs
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skpwg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const partCollection = client.db('robotics_parts').collection('parts');
+
+    app.get('/parts', async (req, res) => {
+      const result = await partCollection.find({}).toArray();
+      res.send(result);
+    })
+
+
+  }
+  finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello from Robotics parts store!')
+});
 
 app.listen(port, () => {
-  console.log(`Manufacture app listening on port ${port}`)
-})
+  console.log(`Rbotics Parts Store listening on port ${port}`)
+});
